c534a26459552cd18ba08ddf80ceac3fdbecefc7
leo-g7 <leo@whyyle.com>
2022-05-05 23:31:56 +0200

    Git init

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+const port = 4000;
+
+app.get('/', (req, res) => {
+  res.send('hello world')
+});
+
+app.listen(port, () => {
+  console.log(`Connected successfully on port ${port}`)
+});
