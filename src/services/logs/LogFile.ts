786166ab526742c73be77773521c5f98a03211fc
leo-g7 <leo@whyyle.com>
2022-05-06 23:54:22 +0200

    Make popular route

--- src/services/logs/LogFile.ts
+++ src/services/logs/LogFile.ts
@@ -1,6 +1,6 @@
 import { DateForamat } from '../../enums'
-import { logs } from '../../types'
 import LogFileParser from './LogFileParser'
+import { logs, query, sortableQuery } from '../../types'
 
 class LogFile {
   private logs: logs = {
@@ -17,7 +17,7 @@ class LogFile {
   }
 
   public CountUniqueUrl = (date: string): number | null => {
-    const dateFormatIndex: number = date.split(/[ :-]+/).length - 1;
+    const dateFormatIndex: number = this.getDateFormatIndex(date)
     let result: number | null = null;
 
     for (const key in DateForamat) {
@@ -26,6 +26,46 @@ class LogFile {
       if (!isNaN(dataFormat)) {
         if (dataFormat === dateFormatIndex && this.logs[dataFormat][date]) {
           result = this.logs[dataFormat][date].count;
+          break
+        }
+      }
+    }
+
+    return result
+  }
+
+  private getDateFormatIndex = (date: string): number => {
+    return date.split(/[ :-]+/).length - 1;
+  }
+
+  public getPopularQueries = (date: string, size: number): query[] => {
+    const dateFormatIndex: number = this.getDateFormatIndex(date)
+    let result: query[] = [];
+
+    for (const key in DateForamat) {
+      const dataFormat: DateForamat = Number(key)
+
+      if (!isNaN(dataFormat)) {
+        if (dataFormat === dateFormatIndex && this.logs[dataFormat][date]) {
+
+          const sortable: sortableQuery[] = [];
+
+          for (const url in this.logs[dataFormat][date].urls) {
+            sortable.push([url, this.logs[dataFormat][date].urls[url].count]);
+          }
+
+          sortable.sort((a, b) => {
+            return b[1] - a[1];
+          });
+
+          const queries: sortableQuery[] = sortable.slice(0, size)
+
+          for (const querie of queries) {
+            result.push({
+              query: querie[0],
+              count: querie[1]
+            })
+          }
           break
         }
       }
